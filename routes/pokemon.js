34dbd01c21d1dc7085fb7a75de72d9c4f21b3ac2
lucas-bui <luan237@gmail.com>
2022-06-10 09:53:40 -0400

    added random endpoint

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,4 +6,8 @@ router.route("/pokemon").get((req, res) => {
   res.status(200).json(pokemonData);
 });
 
+router.route("/pokemon/random").get((req, res) => {
+  randomPok = pokemonData[Math.floor(Math.random() * (pokemonData.length - 1))];
+  res.status(200).json(randomPok);
+});
 module.exports = router;
