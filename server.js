d6aa5638309170547b8033fa2d12acd38f32a49b
Nicole Cevey <nicole.cevey@gmail.com>
2022-06-10 11:31:31 -0400

    Initial commit

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 const app = express();
 const { PORT } = process.env;
 
+app.use(cors());
 app.use("/", pokemon);
 app.use(express.json());
-app.use(cors());
 app.use(express.static("./public"));
 
 app.listen(PORT || 8080, () => {
