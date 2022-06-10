70051639ff3d14f63b13f683eadbe590d517e49c
Lucas <luan237@gmail.com>
2022-06-09 21:52:51 -0400

    updated pokemon data

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+// const pokemon = require("./routes/pokemon");
+
+// const uuid = require("uuid");
+const cors = require("cors");
+const app = express();
+const { PORT } = process.env;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("./public"));
+
+app.listen(PORT || 8080, () => {
+  console.log("Server running on port 8080");
+});
